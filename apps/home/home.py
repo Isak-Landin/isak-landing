38a4b717d8b52eba979c15de6b2229f6748fc1d5
Isak-Landin <utbisalan@gmail.com>
2025-04-25 16:46:12 +0200

    +

--- apps/home/home.py
+++ apps/home/home.py
@@ -4,6 +4,11 @@ from flask import Blueprint, render_template, request, redirect, url_for
 blueprint = Blueprint('home_blueprint', __name__, url_prefix='/')
 
 
-@blueprint.route('/')
+@blueprint.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        # Handle form submission or other POST request logic here
+        # For example, you can redirect to another route after processing
+        return redirect(url_for('home_blueprint.home'))
     return render_template('index.html')
+
