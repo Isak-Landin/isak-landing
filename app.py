d64ae41e8f51730f0b14184b9ac086ba08a73586
Isak-Landin <utbisalan@gmail.com>
2025-08-03 05:12:51 +0200

    +

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from dotenv import load_dotenv
+from extensions import db
 
 # Import blueprints from different apps
 from apps.home.home import blueprint as home_bp
@@ -9,6 +10,8 @@ from apps.contact.contact import blueprint as contact_bp
 from apps.hosting.hosting import blueprint as hosting_bp
 from apps.server.server import blueprint as server_bp
 from apps.support.support import blueprint as support_bp
+from apps.Users.auth import auth_blueprint
+from apps.Users.users import blueprint as users_bp
 from apps.Users.models import User
 
 from flask_login import LoginManager
@@ -22,20 +25,29 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-# Load environment variables
-load_dotenv()
+def create_app():
+    _app = Flask(__name__, template_folder='static/templates', static_folder='static')
 
-app = Flask(__name__, template_folder='static/templates', static_folder='static')
+    login_manager.init_app(_app)
 
-login_manager.init_app(app)
+    # Load environment variables
+    load_dotenv()
 
-app.register_blueprint(home_bp)
-app.register_blueprint(websites_bp)
-app.register_blueprint(about_bp)
-app.register_blueprint(contact_bp)
-app.register_blueprint(hosting_bp)
-app.register_blueprint(server_bp)
-app.register_blueprint(support_bp)
+    _app.register_blueprint(home_bp)
+    _app.register_blueprint(websites_bp)
+    _app.register_blueprint(about_bp)
+    _app.register_blueprint(contact_bp)
+    _app.register_blueprint(hosting_bp)
+    _app.register_blueprint(server_bp)
+    _app.register_blueprint(support_bp)
+    _app.register_blueprint(auth_blueprint)
+    _app.register_blueprint(users_bp)
+
+    # Initialize the database here if needed
+    db.init_app(_app)
+
+    return _app
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    create_app().run(debug=True)
