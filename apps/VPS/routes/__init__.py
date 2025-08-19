91b9c5a544b53928395dfc6938b3224291bcf82b
Isak-Landin <utbisalan@gmail.com>
2025-08-20 00:11:55 +0200

    +

--- apps/VPS/routes/__init__.py
+++ apps/VPS/routes/__init__.py
@@ -4,3 +4,4 @@ from . import success         # noqa: F401
 from . import cancel          # noqa: F401
 from . import billing_portal  # noqa: F401
 from . import webhook         # noqa: F401
+from . import orders
