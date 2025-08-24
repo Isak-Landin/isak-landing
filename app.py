cd8b31f1290c26a194d7dc8cbc67a7a1792cf76f
Isak-Landin <utbisalan@gmail.com>
2025-08-24 22:57:29 +0200

    Depracated old index, implemented new

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ from extensions import db, socketio
 from apps.admin.models import AdminUser
 
 # Import blueprints from different apps
-from apps.home.home import blueprint as home_bp
-from apps.websites.websites import blueprint as websites_bp
-from apps.about.about import blueprint as about_bp
-from apps.contact.contact import blueprint as contact_bp
-from apps.hosting.hosting import blueprint as hosting_bp
-from apps.server.server import blueprint as server_bp
-from apps.support.support import blueprint as support_bp
+# from apps.home.home import blueprint as home_bp
+# from apps.websites.websites import blueprint as websites_bp
+# from apps.about.about import blueprint as about_bp
+# from apps.contact.contact import blueprint as contact_bp
+# from apps.hosting.hosting import blueprint as hosting_bp
+# from apps.server.server import blueprint as server_bp
+# from apps.support.support import blueprint as support_bp
 from apps.Users.auth import auth_blueprint
 from apps.Users.users import blueprint as users_bp
 from apps.Users.models import User
@@ -58,13 +58,6 @@ def create_app():
 
     import apps.chat.socket_events
 
-    _app.register_blueprint(home_bp)
-    _app.register_blueprint(websites_bp)
-    _app.register_blueprint(about_bp)
-    _app.register_blueprint(contact_bp)
-    _app.register_blueprint(hosting_bp)
-    _app.register_blueprint(server_bp)
-    _app.register_blueprint(support_bp)
     _app.register_blueprint(auth_blueprint)
     _app.register_blueprint(users_bp)
     _app.register_blueprint(store_blueprint)
--- apps/home/home.py
+++ apps/home/home.py
@@ -1,14 +1,16 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for
 
+home_blueprint = Blueprint("home_blueprint", __name__, template_folder="../../static/templates")
 
-blueprint = Blueprint('home_blueprint', __name__, url_prefix='/')
+@home_blueprint.route("/")
+def index():
+    return render_template("public/index.html")
 
-
-@blueprint.route('/', methods=['GET', 'POST'])
-def home():
-    if request.method == 'POST':
-        # Handle form submission or other POST request logic here
-        # For example, you can redirect to another route after processing
-        return redirect(url_for('home_blueprint.home'))
-    return render_template('index.html')
-
+# Optional: legacy URL redirects → '/'
+@home_blueprint.route("/about")
+@home_blueprint.route("/contact")
+@home_blueprint.route("/hosting")
+@home_blueprint.route("/server")
+@home_blueprint.route("/websites")
+def legacy_redirect():
+    return redirect(url_for("home_blueprint.index"), code=301)
