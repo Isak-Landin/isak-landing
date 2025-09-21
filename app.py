4a6cf15f71a59827ec651884d725766684b4de62
Isak-Landin <utbisalan@gmail.com>
2025-09-21 16:06:37 +0200

    +

--- app.py
+++ app.py
@@ -182,6 +182,7 @@ def set_security_headers(resp):
             "form-action 'self'; "
             "frame-ancestors 'self'; "
             "object-src 'none'"
+            "base-uri 'self';"
         )
     return resp
 
